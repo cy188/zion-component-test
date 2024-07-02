354ad6552d72a6adfa85e879b67657d0d4b7e9e4
cheny <1316967697@qq.com>
2024-07-02 22:34:33 +0800

    测试112

--- src/components/AiPptDoc/AiPptDoc.tsx
+++ src/components/AiPptDoc/AiPptDoc.tsx
@@ -1,29 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-empty-pattern */
 
-import { genAiPptDocSrc } from './aiPptDocSrc';
+import { genAiPptDocSrc } from "./aiPptDocSrc";
 
-interface AiPptDocProps {
-  globalData: Record<string, any>;
-  setGlobalData: (newValue: any) => void;
-  value?: string;
-}
+// interface AiPptDocProps {
+//   globalData: Record<string, any>;
+//   setGlobalData: (newValue: any) => void;
+//   value?: string;
+// }
 
 const iframeInlineStyle: React.CSSProperties = {
-  border: '0px solid',
-  height: '100%',
-  width: '100%',
+  border: "0px solid",
+  height: "100%",
+  width: "100%",
 };
 
-export function AiPptDoc({ globalData }: AiPptDocProps) {
-  return (
-    <iframe
-      style={iframeInlineStyle}
-      srcDoc={genAiPptDocSrc({
-        appkey: globalData.appkey,
-        channel: globalData.channel,
-        code: globalData.code,
-      })}
-    />
-  );
+export function AiPptDoc() {
+  return <iframe style={iframeInlineStyle} srcDoc={genAiPptDocSrc()} />;
 }
